57398c0f7a919276f981dc28ca34a174ec9610bc
SrivyshnaviKoduri <srivyshnavikoduri@gmail.com>
2022-04-19 13:42:31 +0530

    Initialized Repository

---
+++ src/main/java/com/bookings/stayclassy/StayclassyApplication.java
@@ -0,0 +1,13 @@
+package com.bookings.stayclassy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayclassyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayclassyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookings/stayclassy/StayclassyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookings.stayclassy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayclassyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
